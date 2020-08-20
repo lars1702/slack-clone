ff8f58dc8b070d3447538ebe40beaea06836d695
Lars1702 <Lars1702@gmail.com>
2020-08-20 11:19:03 +0200

    first commit

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -1,5 +1,12 @@
-body {
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+* {
   margin: 0;
+  padding: 0;
+}
+
+body {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
@@ -11,3 +18,6 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+`
+
+export default GlobalStyle
